3b3202eefa8719db0f74699fe9f55ed3f35afa58
vishal5394 <vishaljadyal@gmail.com>
2022-08-03 20:59:32 +0530

    Added: Throw Null Point Exception

--- MoodAnalyzer.java
+++ MoodAnalyzer.java
@@ -1,19 +1,27 @@
 package com.BridgeLabz.MoodAnalyzer;
 
 public class MoodAnalyzer {
-	   private String message;
-	    public MoodAnalyzer(String message) {
-	        this.message = message;
-	    }
+    private String message;
 
-	    public String analyseMood() {
-	        try {
-	            if (message.contains("Sad"))
-	                return "SAD";
-	            else
-	                return "HAPPY";
-	        }catch (NullPointerException A){
-	            return "HAPPY";
-	        }
-	    }
-	}
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood(String message) throws MoodAnalysisException {
+        this.message = message;
+        return analyseMood();
+    }
+
+    public String analyseMood() throws MoodAnalysisException {
+        try {
+            if (message.length() == 0)
+                throw new MoodAnalysisException(MoodAnalysisException.exceptionType.ENTERED_EMPTY, "Please enter Proper Mood");
+            if (message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (NullPointerException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.exceptionType.ENTERED_NULL, "Please enter Proper Mood");
+        }
+    }
+}
